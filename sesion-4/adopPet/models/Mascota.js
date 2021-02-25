da33fe1d889f92b0f9e7a4ca43be983b98621ccd
Alfredo <apaz0@ucol.mx>
2021-02-24 22:03:44 -0600

    Sesion-4 fixed

--- sesion-4/adopPet/models/Mascota.js
+++ sesion-4/adopPet/models/Mascota.js
@@ -5,7 +5,7 @@ class Mascota{
         this.id = id;
         this.nombre = nombre; // nombre de la mascota (o titulo del anuncio)
         this.categoria = categoria; // perro | gato | otro
-        this.fotos = fotos; // links a las fotografías
+        //this.fotos = fotos; // links a las fotografías
         this.descripcion = descripcion; // descripción del anuncio
         this.anunciante = anunciante; // contacto con la persona que anuncia al animalito
         this.ubicacion = ubicacion; // muy importante
