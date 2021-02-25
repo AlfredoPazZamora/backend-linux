cabe47090b940e6ac5c74f9cdfe6a829dda2380a
Alfredo <apaz0@ucol.mx>
2021-02-24 20:21:46 -0600

    Sesion-4 configuracion app

--- sesion-4/adopPet/app.js
+++ sesion-4/adopPet/app.js
@@ -1,1 +1,28 @@
+// Importamos las bibliotecas necesarias
+let express = require('express'),
+    bodyParser = require('body-parser'),
+    cors = require('cors');
 
+// Objeto global de la app
+let app = express();
+
+// configuración de middlewares
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Manejamos los errores 404
+//next (siguiente middelware), 
+//req - res (solicitud - respuesta)
+app.use((req,res,next)=> { 
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+//Iniciando el servidor
+let port = process.env.PORT || 3000;
+
+let server = app.listen(port, ()=>{
+    console.log('Escuchando desde el puerto ' + server.address().port);
+})
