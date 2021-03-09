1deaecdf26a23d8665c68d7c74b3df149c1d86da
Alfredo <apaz0@ucol.mx>
2021-03-08 20:04:14 -0600

    Uso de validadores

--- sesion-4/adopPet/models/Usuario.js
+++ sesion-4/adopPet/models/Usuario.js
@@ -19,21 +19,42 @@
   
 // module.exports = Usuario;
 //MONGOOSE
-const mongoose = require('mongoose');
-//Definiendo el objeto UsuarioSchema con el constructor Schema.
-//Definiendo cada campo con su respectivo tipo de dato.
-const UsuarioSchema = new mongoose.Schema({      
-    username: String,                              
-    nombre: String,
-    apellido: String, 
-    email: String,
-    password: String,
+// Usuario.js
+const mongoose = require('mongoose');                         //Importando mongoose.
+const uniqueValidator = require("mongoose-unique-validator"); //Importando módulo mongoose-unique-validator, pendiente de instalar.
+const crypto = require('crypto');                             //Importando módulo crypto, pendiente de instalar.
+const jwt = require('jsonwebtoken');                          //Importando módulo jsonwebtoken, pendiente de instalar.
+const secret = require('../config').secret;                   // ????
+
+//Definiendo cada campo con sus tipo sde datos y las validaciones sobre este.
+const UsuarioSchema = new mongoose.Schema({                   
+    username: {                                                  
+        type: String,
+        unique: true, //este campo no se puede repetir
+        lowercase: true,
+        required: [true, "no puede estar vacío"],
+        match: [/^[a-zA-Z0-9]+$/, "es inválido"],
+        index: true,
+    },                                           
+    nombre: { type: String, required: true },
+    apellido: { type: String, required: true },
+    email: {
+        type: String,
+        unique: true, //este campo no se puede repetir
+        lowercase: true,
+        required: [true, "no puede estar vacío"],
+        match: [/\S+@\S+\.\S+/, "es inválido"],
+        index: true,
+    },
     ubicacion: String,
     telefono: String,
     bio: String,
     foto: String,
-    tipo: String,
-}, { timestamps: true });  
- 
- //Define el modelo Usuario, utilizando el esquema UsuarioSchema.
- mongoose.model("Usuario", UsuarioSchema);        
+    tipo: { type: String, enum: ['normal', 'anunciante'] },
+    hash: String, //este campo se utilizará para la sesión
+    salt: String, //este campo se utilizará para la sesión
+},{ timestamps: true });
+
+// usando plugin de validación para que no se repitan correos ni usernames
+UsuarioSchema.plugin(uniqueValidator, { message: "Ya existe" }); 
+mongoose.model("Usuario", UsuarioSchema);    //Define el modelo Usuario, utilizando el esquema UsuarioSchema.
