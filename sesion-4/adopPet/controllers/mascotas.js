cb8fce874150306d295208556ab1c627f49ba14d
Alfredo <apaz0@ucol.mx>
2021-03-10 17:45:42 -0600

    arreglo modelos y controladores

--- sesion-4/adopPet/controllers/mascotas.js
+++ sesion-4/adopPet/controllers/mascotas.js
@@ -1,31 +1,35 @@
-let Mascota = require('../models/Mascota');
+const mongoose = require('mongoose')
+const Mascota = mongoose.model('Mascota')
 
-function crearMascota(req, res){
-    let mascota = new Mascota(req.body);
-    res.status(201).send(mascota);
-}
+function crearMascota(req, res, next) {
+    var mascota = new Mascota(req.body)
+    mascota.anunciante = req.usuario.id
+    mascota.estado = 'disponible'
+    mascota.save().then(mascota => {
+        res.status(201).send(mascota)
+    }).catch(next)
+};
 
-function obtenerMascotas(req, res){
-    let mascota1 = new Mascota(1 , 'FUFFY', 'perro', 'Perro bonito y bien portado', 1234567890, 'In your mind');
-    let mascota2 = new Mascota(2 , 'MISIFUS', 'gato', 'otro gato', 0987654321, 'la vecindad');
+function obtenerMascotas(req, res, next) {
+    Mascota.find().then(mascotas=>{
+      res.send(mascotas)
+    }).catch(next)
+  }
 
-    res.send([mascota1, mascota2]);
-}
-
-function modificarMascota(req, res){
-    let mascota1 = new Mascota(req.params.id, 'FUFFY', 'perro', 'Perro bonito y bien portado', 1234567890, 'In your mind');
-    let modificaciones = req.body;
-    mascota1 = { ...mascota1, ...modificaciones }
-    res.send(mascota1)
-}
-
-function eliminarMascota(req, res){
-    res.status(200).send(`Tu mascota con id ${req.params.id} fue eliminado :(`);
-}
+function obtenerMascotas(req, res, next) {
+    if(req.params.id){
+      Mascota.findById(req.params.id)
+                .populate('anunciante', 'username nombre apellido bio foto').then(mascotas => {
+            res.send(mascotas)
+          }).catch(next)
+    } else {
+        Mascota.find().then(mascotas=>{
+            res.send(mascotas)
+        }).catch(next)
+    }
+};
 
 module.exports = {
     crearMascota,
-    obtenerMascotas,
-    modificarMascota,
-    eliminarMascota
+    obtenerMascotas
 }
--- sesion-4/adopPet/routes/mascotas.js
+++ sesion-4/adopPet/routes/mascotas.js
@@ -4,11 +4,13 @@ const {
     obtenerMascotas,
     modificarMascota,
     eliminarMascota
-} = require('../controllers/mascotas');
+} = require('../controllers/mascotas')
+var auth = require('./auth');
 
-router.get('/', obtenerMascotas);
-router.post('/', crearMascota);
-router.put('/:id', modificarMascota);
-router.delete('/:id', eliminarMascota);
+router.get('/', auth.opcional,obtenerMascotas)
+router.get('/:id', auth.opcional, obtenerMascotas)// nuevo endpoint con todos los detalles de mascota
+router.post('/', auth.requerido, crearMascota)
+router.put('/:id',auth.requerido, modificarMascota)
+router.delete('/:id',auth.requerido, eliminarMascota)
 
 module.exports = router;
